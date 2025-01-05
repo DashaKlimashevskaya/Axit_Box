fbcca8b88b4e865825c54a08679e63245a6f7ac4
DashaKlimashevskaya <raricompany1824@gmail.com>
2025-01-05 16:24:23 +0300

    fix: rename variables

--- JS/mobile-menu.js
+++ JS/mobile-menu.js
@@ -2,19 +2,17 @@
 
 const clickButtonOpen = document.querySelectorAll('.header .container .header__menu');
 const headerButtonClose = document.querySelectorAll('.header .container .header__close-menu');
-const dropDownMenu = document.querySelectorAll('.header .header__mobile-navigation');
-const bannerTittle =  document.querySelectorAll('.banner .container');
-const bannerText =  document.querySelectorAll('.banner .container .banner__text');
-const bannerBtn =  document.querySelectorAll('.banner .container .banner__btn');
+const leftMenu = document.querySelectorAll('.header .header__mobile-navigation');
+const bannerContainer =  document.querySelectorAll('.banner .container');
 
 clickButtonOpen[0].onclick = function(){
 
-    if(dropDownMenu[0].classList.contains('inaction')){
-        dropDownMenu[0].classList.remove('inaction');
-        dropDownMenu[0].classList.add('action');
+    if(leftMenu[0].classList.contains('inaction')){
+        leftMenu[0].classList.remove('inaction');
+        leftMenu[0].classList.add('action');
     }else{
-        dropDownMenu[0].classList.remove('action');
-        dropDownMenu[0].classList.add('inaction');
+        leftMenu[0].classList.remove('action');
+        leftMenu[0].classList.add('inaction');
     }
 
     if(clickButtonOpen[0].classList.contains('active')){
@@ -33,26 +31,14 @@ clickButtonOpen[0].onclick = function(){
         headerButtonClose[0].classList.add('inactive');
     }
 
-    if(bannerTittle[0].classList.contains('active')){
-        // banner tittle
-        bannerTittle[0].classList.remove('active');
-        bannerTittle[0].classList.add('inactive');
-        // banner text
-        bannerText[0].classList.remove('active');
-        bannerText[0].classList.add('inactive');
-        // banner btn
-        bannerBtn[0].classList.remove('active');
-        bannerBtn[0].classList.add('inactive');
+    if(bannerContainer[0].classList.contains('active')){
+        // banner container
+        bannerContainer[0].classList.remove('active');
+        bannerContainer[0].classList.add('inactive');
     }else{
-        // banner tittle
-        bannerTittle[0].classList.remove('inactive');
-        bannerTittle[0].classList.add('active');
-        // banner text
-        bannerText[0].classList.remove('inactive');
-        bannerText[0].classList.add('active');
-         // banner btn
-         bannerBtn[0].classList.remove('inactive');
-         bannerBtn[0].classList.add('active');
+        // banner container
+        bannerContainer[0].classList.remove('inactive');
+        bannerContainer[0].classList.add('active');
     }
 }
 
@@ -74,33 +60,21 @@ headerButtonClose[0].onclick = function()
             clickButtonOpen[0].classList.add('inactive');
         }
 
-        if(dropDownMenu[0].classList.contains('action')){
-            dropDownMenu[0].classList.remove('action');
-            dropDownMenu[0].classList.add('inaction');
+        if(leftMenu[0].classList.contains('action')){
+            leftMenu[0].classList.remove('action');
+            leftMenu[0].classList.add('inaction');
         }else{
-            dropDownMenu[0].classList.remove('inaction');
-            dropDownMenu[0].classList.add('action');
+            leftMenu[0].classList.remove('inaction');
+            leftMenu[0].classList.add('action');
         }
 
-        if(bannerTittle[0].classList.contains('inactive') && bannerText[0].classList.contains('inactive') && bannerBtn[0].classList.contains('inactive')){
-            // banner tittle
-            bannerTittle[0].classList.remove('inactive');
-            bannerTittle[0].classList.add('active');
-            // banner text
-            bannerText[0].classList.remove('inactive');
-            bannerText[0].classList.add('active');
-            // banner btn
-            bannerBtn[0].classList.remove('inactive');
-            bannerBtn[0].classList.add('active');
+        if(bannerContainer[0].classList.contains('inactive')){
+            // banner container
+            bannerContainer[0].classList.remove('inactive');
+            bannerContainer[0].classList.add('active');
         }else{
-            // banner tittle
-            bannerTittle[0].classList.remove('active');
-            bannerTittle[0].classList.add('inactive');
-            // banner text
-            bannerText[0].classList.remove('active');
-            bannerText[0].classList.add('inactive');
-             // banner btn
-             bannerBtn[0].classList.remove('active');
-             bannerBtn[0].classList.add('inactive');
+            // banner Container
+            bannerContainer[0].classList.remove('active');
+            bannerContainer[0].classList.add('inactive');
         }
     }
